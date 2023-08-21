35e8f55ac972e217a8aaf885072c28737e6a4438
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-08-21 21:00:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 locations = []  # List to store locations
+location_id = 0  # Unique ID for each location
 
 @app.route('/save_location', methods=['POST'])
 def save_location():
+    global location_id
     data = request.json
     latitude = data.get('latitude')
     longitude = data.get('longitude')
     
-    locations.append({"latitude": latitude, "longitude": longitude})
+    location_id += 1
+    locations.append({"id": location_id, "latitude": latitude, "longitude": longitude})
     
     return jsonify({"message": "Location saved successfully!"})
 
