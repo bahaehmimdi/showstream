684c874a71488be04033a9dbdad09142b69adbb2
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-08-22 19:38:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 
-locations = {}  # List to store locations
-location_id = 0  # Unique ID for each location
+# In-memory database for simplicity
+events = []
 
-@app.route('/save_location', methods=['POST'])
-def save_location():
-    global location_id
-    data = request.json
-    latitude = data.get('latitude')
-    longitude = data.get('longitude')
-    name = data.get('name')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        start_time = request.form.get('start_time')
+        end_time = request.form.get('end_time')
+        event_string = request.form.get('event_string')
+        events.append({
+            'start_time': start_time,
+            'end_time': end_time,
+            'event_string': event_string
+        })
+        return redirect(url_for('index'))
+    return render_template('index.html', events=events)
 
-    location_id += 1
-    locations[name]={"id": location_id, "latitude": latitude, "longitude": longitude,"name":name}
-    
-    return jsonify({"message": "Location saved successfully!"})
+@app.route('/current_event', methods=['GET'])
+def current_event():
+    now = datetime.now().isoformat()
+    for event in events:
+        if event['start_time'] <= now <= event['end_time']:
+            return jsonify(event)
+    return jsonify({'event_string': 'nothing'})
 
-@app.route('/get_locations', methods=['GET'])
-def get_locations():
-   try: 
-    return jsonify(locations)
-   except Exception as me:
-       return str(me)
-@app.route('/')
-def bonjour():
-    return "Bonjour c est bahae el hmimdi le devlopeur" +str(locations)
+@app.route('/display', methods=['GET'])
+def display():
+    return render_template('display.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 if __name__ == '__main__':
     app.run(debug=True)
